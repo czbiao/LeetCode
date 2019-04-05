02716140d7d34c6ab1d67ef7d2008a6d0ae39689
chenzebiao <13413423055@163.com>
2019-04-05 17:15:31 +0800

    improve  001-two-sum
    improve  001-two-sum

--- Solution/001 Two Sum/001-two-sum.js
+++ Solution/001 Two Sum/001-two-sum.js
@@ -2,10 +2,28 @@
  * @Author: chenzebiao 
  * @Date: 2019-04-05 14:11:20 
  * @Last Modified by: chenzebiao
- * @Last Modified time: 2019-04-05 14:48:10
+ * @Last Modified time: 2019-04-05 17:14:30
  */
 
 /**
+ * 暴力法
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+
+    }
+};
+
+/**
+ * 利用js的indexOf方法，此方法时间复杂度最大值O(n^2)
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -19,7 +37,61 @@ var twoSum = function (nums, target) {
     }
 };
 
+/**
+ * Map
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const arrMap = new Map();
+    for (const [index, value] of Object.entries(nums)) {
+        arrMap.set(value, index);
+    }
+    for (const [index, value] of Object.entries(nums)) {
+        const complement = target - value;
+        if (arrMap.has(complement) && arrMap.get(complement) !== index) {
+            return [Number(index), Number(arrMap.get(complement))];
+        }
+    }
+};
+
+/**
+ * 改进后的Map，在插入新数据前，就可以进行判断
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const arrMap = new Map();
+    for (const [index, value] of Object.entries(nums)) {
+        const complement = target - value;
+        if (arrMap.has(complement)) {
+            return [Number(arrMap.get(complement)), Number(index)];
+        }
+        arrMap.set(value, index);
+    }
+};
+
+/**
+ * 对象存储
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (complement in obj) {
+            return [obj[complement], i];
+        }
+        obj[nums[i]] = i;
+    }
+    return null;
+};
+
 
 const nums = [2, 7, 11, 15];
-const target = 13;
+const target = 9;
 console.log(twoSum(nums, target));

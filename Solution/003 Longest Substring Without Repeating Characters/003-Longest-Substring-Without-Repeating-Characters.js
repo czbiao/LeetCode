df4741b095efabdc8243251d42903b04a3a9f0c1
chenzebiao <13413423055@163.com>
2019-04-12 23:52:20 +0800

    Update 003-Longest-Substring-Without-Repeating-Characters.js

--- Solution/003 Longest Substring Without Repeating Characters/003-Longest-Substring-Without-Repeating-Characters.js
+++ Solution/003 Longest Substring Without Repeating Characters/003-Longest-Substring-Without-Repeating-Characters.js
@@ -2,13 +2,26 @@
  * @Author: chenzebiao 
  * @Date: 2019-04-10 23:26:21 
  * @Last Modified by: chenzebiao
- * @Last Modified time: 2019-04-10 23:34:42
+ * @Last Modified time: 2019-04-12 23:52:17
  */
 
 /**
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    
+var lengthOfLongestSubstring = function (s) {
+    const strMap = new Map();
+    let max = 0;
+    for (let j = 0, i = 0; j < s.length; j++) {
+        if (strMap.get(s.charAt(j))) {
+            i = Math.max(strMap.get(s.charAt(j)), i);
+        }
+        max = Math.max(max, j - i + 1);
+        strMap.set(s.charAt(j), j + 1);
+    }
+    return max;
 };
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
+console.log(lengthOfLongestSubstring('bbbbb'));
+console.log(lengthOfLongestSubstring('pwwkew'));

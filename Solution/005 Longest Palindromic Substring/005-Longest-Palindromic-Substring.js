cd4676ad5b2d2cc717d01b5cd7c54ab73c3339b1
chenzebiao <13413423055@163.com>
2019-04-15 23:55:30 +0800

    add 005 Longest Palindromic Substring

---
+++ Solution/005 Longest Palindromic Substring/005-Longest-Palindromic-Substring.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: chenzebiao 
+ * @Date: 2019-04-15 23:36:09 
+ * @Last Modified by:   chenzebiao 
+ * @Last Modified time: 2019-04-15 23:36:09 
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+};
